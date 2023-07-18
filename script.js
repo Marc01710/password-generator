09ea7ce9b4a968b4ecc99df7e79f38a6a2b71bf8
WeenoCoding <hernandez17marc@gmail.com>
2023-07-17 22:00:02 -0700

    added generatePassword function, next thing to do is add confirms for characters

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordLength = prompt("Enter password length between 8 and 128 characters.");
+  passwordLength = parseInt(passwordLength, 10);
+  console.log(passwordLength);
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length.");
+    generatePassword();
+  }
+  var specialCharacters = confirm("Include special characters?");
+  var 
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
