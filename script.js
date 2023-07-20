d733e33b26a8225defd27bfdad532669b4b51c3d
WeenoCoding <hernandez17marc@gmail.com>
2023-07-19 18:37:24 -0700

    added variables/arrays and if statements in generatepassword function

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "<", ">", "[", "]", "{", "}", "/", "?"];
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = lowerCase.toUpperCase().split("");
+var numbers = "1234567890";
+var emptyArray = [];
+
+
 
 function generatePassword() {
   var passwordLength = prompt("Enter password length between 8 and 128 characters.");
   passwordLength = parseInt(passwordLength, 10);
-  console.log(passwordLength);
+  // console.log(passwordLength);
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length.");
     generatePassword();
   }
-  var specialCharacters = confirm("Include special characters?");
-  var 
+  if (confirm("Include special characters?")) {
+    emptyArray.concat(specialCharacters);
+  };
+  if (confirm("Include lower case lettering?")) {
+    emptyArray.concat(lowerCase.split(""));
+  };
+  if (confirm("Include upper case lettering?")) {
+    emptyArray.concat(upperCase);
+  };
+  if (confirm("Include numbers?")) {
+    emptyArray.concat(numbers.split(""));
+  };
+  return;
+};
+
+function randomize(passwordLength) {
+  var returnValue = "";
+  for (var i = 0; i < passwordLength; i++) {
+    returnValue += emptyArray[Math.floor(Math.random() * emptyArray.length)];
+    
+  }
 }
+
+// code for if statements above for the variables are true or not
 
 
 // Write password to the #password input
