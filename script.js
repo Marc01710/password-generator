5ca55e315372e225e5cdf26ebeadcded1fed32e4
WeenoCoding <hernandez17marc@gmail.com>
2023-07-19 18:57:01 -0700

    got password to generate in empty arrays

--- script.js
+++ script.js
@@ -11,35 +11,45 @@ var emptyArray = [];
 function generatePassword() {
   var passwordLength = prompt("Enter password length between 8 and 128 characters.");
   passwordLength = parseInt(passwordLength, 10);
+  emptyArray = [];
   // console.log(passwordLength);
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Please enter a valid password length.");
     generatePassword();
   }
   if (confirm("Include special characters?")) {
-    emptyArray.concat(specialCharacters);
+    emptyArray = emptyArray.concat(specialCharacters);
   };
   if (confirm("Include lower case lettering?")) {
-    emptyArray.concat(lowerCase.split(""));
+    emptyArray = emptyArray.concat(lowerCase.split(""));
   };
   if (confirm("Include upper case lettering?")) {
-    emptyArray.concat(upperCase);
+    emptyArray = emptyArray.concat(upperCase);
   };
   if (confirm("Include numbers?")) {
-    emptyArray.concat(numbers.split(""));
+    emptyArray = emptyArray.concat(numbers.split(""));
   };
+  // console.log(emptyArray);
+  randomize(passwordLength, emptyArray);
   return;
+
+ 
 };
 
-function randomize(passwordLength) {
+
+
+function randomize(passwordLength, emptyArray) {
   var returnValue = "";
   for (var i = 0; i < passwordLength; i++) {
     returnValue += emptyArray[Math.floor(Math.random() * emptyArray.length)];
-    
-  }
-}
+    };
+    // console.log(returnValue);
+    // console.log(passwordLength);
+    // console.log(emptyArray);
+    return returnValue;
+};
 
-// code for if statements above for the variables are true or not
+
 
 
 // Write password to the #password input
